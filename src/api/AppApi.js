e2caeeaa5b18b5160e479b2b572ec00fbc0a239d
Swapnil Mali <swapnilmali@yahoo.com>
2019-04-11 23:02:59 +0530

    Comment

--- src/api/AppApi.js
+++ src/api/AppApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { API_KEY, BASE_API_URL } from "../config";
 
+// Created axios instance.
+//Attached cacel token to cancel the async call when users move to different page.
 export const getInstance = () => {
   const instance = axios.create({
     baseURL: BASE_API_URL,
